dc95484edfc29457a0c13b44956fddf3a52feb7b
Brian <brian.c.mcmahan@gmail.com>
2016-10-26 18:29:32 -0400

    resyncing

--- utils/makepush.sh
+++ utils/makepush.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-python bibstack.py make all && cd .. && git add --all && git commit -am 'resyncing' && git push -u origin gh-pages
+python bibstack.py make all
+
+cd ..
+git add --all
+git commit -am 'resyncing'
+git push -u origin gh-pages
